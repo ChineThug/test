e2229f7fd3328c21887c13ec12061722cd2d229e
Fxu <Francoisxu2000@gmail.com>
2020-02-16 15:39:21 +0100

    test

---
+++ Projet/platforms/android/app/build/intermediates/merged_assets/debug/out/www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "com.bunkerpalace.cordova.YoutubeVideoPlayer.YoutubeVideoPlayer",
+      "file": "plugins/com.bunkerpalace.cordova.YoutubeVideoPlayer/plugins/com.bunkerpalace.cordova.YoutubeVideoPlayer/www/YoutubeVideoPlayer.js",
+      "pluginId": "com.bunkerpalace.cordova.YoutubeVideoPlayer",
+      "clobbers": [
+        "YoutubeVideoPlayer"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "com.bunkerpalace.cordova.YoutubeVideoPlayer": "1.0.5"
+  };
+});
